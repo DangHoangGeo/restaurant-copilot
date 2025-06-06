65c8df69d1a222c07f21bb4a88a4a2aa378f3077
danghoanggeo <dang.hoang.geo@gmail.com>
2025-06-06 20:58:06 +0900

    Building IOS app

---
+++ mobileapp/SOrder.jp/SOrder.jp/SOrder_jpApp.swift
@@ -0,0 +1,17 @@
+//
+//  SOrder_jpApp.swift
+//  SOrder.jp
+//
+//  Created by Dang Hoang on 2025/06/06.
+//
+
+import SwiftUI
+
+@main
+struct SOrder_jpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
